b7f3045ff40b4bc5fb7076b4e8a3a78ccc65bd1f
Patrick Carvalho <carvalhopatrick37@gmail.com>
2018-10-29 02:17:57 -0300

    Update EnvioCliente/Config.h

---
+++ EnvioCliente/Config.h
@@ -0,0 +1,30 @@
+// Wifi settings
+#define ssid "PLKKPT"
+#define password "LaranjaM3canica"
+#define wirelessHostname "Techmaker-Follower_Counter"
+
+// OTA update settings
+#define otaPassword "123"
+
+// Web server settings
+#define webServerUsername "Zeeshan"
+#define webServerPassword "123123123"
+
+// YouTube settings
+#define youtubeChannelId "AIzaSyAuymzF4s6irfTXdD5yF-DZ_PGGzM4j3r4"
+#define youtubeApiKey "xxxxxxxxxxxxxx"
+
+// Facebook settings
+#define facebookPageId "293445158049843"
+#define facebookAppId "2099378660125688"
+#define facebookAppSecret "05ad93087889652b86fcbfd90257e263"
+#define facebookAccessToken "EAAd1X69JKZCgBALw4iIqOntxGxXCZCI3B0uAuwsYc5koUKE3MeWVFBabVvZBq9EZBzvPT5wyyOS3d1OObAZBLZB87YEQYOOxOT3wKooo3km4JhAjGtZBLffPc9ZB5nvx97E4CHba66ys0SPk2gugtRp1HOIuBEkxEv6wVNz2NeQMQeYk1jxgdoIqKXpDTeSjUodYiItKAMwgcgZDZD"
+
+// Twitter settings
+#define twitterPageName "leotechmaker"
+
+// Instagram settings
+// https://smashballoon.com/instagram-feed/find-instagram-user-id/
+#define instagramPageId "2368412736"
+#define instagramAccessToken "xxxxxxxxxxxxxx"
+#define instagramSSLCertificateFootprint "b98ffcc91caa900719fc817334b4736384dd294e"
